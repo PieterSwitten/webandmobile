555e9c4ae0e850e8d4f2b43e977e7a3830daa1d4
Dylan <dylangomes@live.Be>
2015-12-15 11:07:37 +0100

    Added login page

--- WebProjectSym/src/AppBundle/Controller/DefaultController.php
+++ WebProjectSym/src/AppBundle/Controller/DefaultController.php
@@ -8,14 +8,49 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="homeroute")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        return $this->render('default/index.html.twig');
     }
+    /**
+     * @Route("/adminpage", name="adminroute")
+     */
+    public function adminAction(Request $request)
+    {
+        return new Response("adminpage<br/>");
+    }
+    /**
+     * @Route("/userpage", name="userroute")
+     */
+    public function userAction(Request $request)
+    {
+        return new Response("userpage<br/>");
+    }
+    /**
+     * @Route("/login", name="loginroute")
+     */
+    public function loginAction(Request $request)
+    {
+        return $this->render('default/login.html.twig');
+    }
+
+    /**
+     * @Route("/login_check", name="checkroute")
+     */
+    public function loginCheckAction()
+    {
+        // NB hier geen code: het framework voorziet de controles/acties
+    }
+    /**
+     * @Route("/quit", name="quitroute")
+     */
+    public function quitAction(Request $request)
+    {
+        // NB hier geen code: het framework voorziet de acties
+    }
+
 }
