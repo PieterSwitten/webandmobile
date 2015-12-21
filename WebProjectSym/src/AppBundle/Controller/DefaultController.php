563bc125a17c43729e691e1e4b8738975026df4a
Dylan <dylangomes@live.Be>
2015-12-21 17:06:30 +0100

    Added register users

--- WebProjectSym/src/AppBundle/Controller/DefaultController.php
+++ WebProjectSym/src/AppBundle/Controller/DefaultController.php
@@ -7,6 +7,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use AppBundle\Form\UserType;
 
 class DefaultController extends Controller
 {
@@ -53,6 +56,34 @@ class DefaultController extends Controller
      */
     public function quitAction(Request $request) { }
 
+
+    /**
+     * @Route("/registerpage", name="registerroute")
+     */
+    public function registerAction(Request $request) {
+        $password = "";
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted()&&$form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $user->setRolesString("ROLE_USER");
+            $password = $user->getPassword();
+            $encoder = $this->container->
+            get('security.password_encoder');
+            $encoded = $encoder->encodePassword($user,
+                $password);
+            $user->setPassword($encoded);
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash('User',$user);
+            return $this->redirectToRoute('homeroute');
+        } else {
+            return $this->render(
+                'default/new.html.twig',
+                array('form' => $form->createView()));
+        }
+    }
     /**
      * @Route("/makeusers")
      */
@@ -73,5 +104,7 @@ class DefaultController extends Controller
         $em->flush();
         return new Response('Created user');
     }
+
+
 
 }
