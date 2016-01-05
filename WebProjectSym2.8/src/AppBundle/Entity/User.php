0eaf6090683484dce90e1830e1a2accaed27c44d
Dylan <dylangomes@live.Be>
2016-01-05 12:19:37 +0100

    added controller & changed db

--- WebProjectSym2.8/src/AppBundle/Entity/User.php
+++ WebProjectSym2.8/src/AppBundle/Entity/User.php
@@ -3,7 +3,6 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * User
@@ -11,7 +10,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
  * @ORM\Table(name="user")
  * @ORM\Entity
  */
-class User implements UserInterface, \Serializable
+class User
 {
     /**
      * @var integer
@@ -42,105 +41,6 @@ class User implements UserInterface, \Serializable
      * @ORM\Column(name="rolesstring", type="string", length=255, nullable=false)
      */
     private $rolesstring;
-
-
-    public function serialize()
-    {
-        return serialize(array(
-            $this->id,
-            $this->username,
-            $this->password,
-            $this->rolesstring
-        ));
-    }
-
-    public function unserialize($serialized)
-    {
-        list (
-            $this->id,
-            $this->user,
-            $this->password,
-            $this->rolesstring
-            ) = unserialize($serialized);
-    }
-
-    /**
-     * @return int
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param int $id
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-
-    /**
-     * @return string
-     */
-    public function getUsername()
-    {
-        return $this->username;
-    }
-
-    /**
-     * @param string $username
-     */
-    public function setUsername($username)
-    {
-        $this->username = $username;
-    }
-
-    /**
-     * @return string
-     */
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    /**
-     * @param string $password
-     */
-    public function setPassword($password)
-    {
-        $this->password = $password;
-    }
-
-    /**
-     * @return string
-     */
-    public function getRolesstring()
-    {
-        return $this->rolesstring;
-    }
-
-    /**
-     * @param string $rolesstring
-     */
-    public function setRolesstring($rolesstring)
-    {
-        $this->rolesstring = $rolesstring;
-    }
-
-    public function getRoles()
-    {
-        return split(' ',$this->rolesstring);
-    }
-
-    public function getSalt()
-    {
-        return null;
-    }
-
-    public function eraseCredentials()
-    {
-    }
 
 
 }
