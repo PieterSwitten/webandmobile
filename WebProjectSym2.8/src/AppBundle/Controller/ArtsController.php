cb33672335f95e0a9b72b6da33665e5f5fabfff5
Dylan <dylangomes@live.Be>
2016-01-12 09:29:48 +0100

    artscontrollers

--- WebProjectSym2.8/src/AppBundle/Controller/ArtsController.php
+++ WebProjectSym2.8/src/AppBundle/Controller/ArtsController.php
@@ -13,17 +13,5 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ArtsController extends Controller {
 
-    /**
-     * @Route("/artsprofielbeheer", name="artsroute")
-     */
-    public function indexAction(Request $request)
-    {
-        $user = $this->get('security.token_storage')->getToken()->getUser();
-        $id = $user->getId();
-
-        $arts = $this->getDoctrine()
-            ->getRepository('AppBundle:Arts')
-            ->find($id);
-    }
 
 }
