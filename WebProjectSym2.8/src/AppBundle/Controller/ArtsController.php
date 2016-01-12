cef86feb0c77b47fe32eca34e8585c49410ca1d2
Dylan <dylangomes@live.Be>
2016-01-12 10:30:27 +0100

    database connection for arts

--- WebProjectSym2.8/src/AppBundle/Controller/ArtsController.php
+++ WebProjectSym2.8/src/AppBundle/Controller/ArtsController.php
@@ -7,11 +7,44 @@
  */
 
 namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\User;
+use AppBundle\Entity\Arts;
 
 class ArtsController extends Controller {
 
+    /**
+     * @Route("/Index", name="artsroute")
+     */
+    public function indexAction(Request $request)
+    {
+
+
+        return new Response("ArtsPanel<br/>");
+    }
+
+    /**
+     * @Route("/artsprofielbeheer", name="artsprofielroute")
+     */
+    public function ArtsprofielBeheerAction(Request $request) {
+
+        $user = new User();
+        $arts = new Arts();
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        $id = $user->getId();
+
+        $repository = $this->getDoctrine()
+            ->getRepository('AppBundle:Arts');
+
+        $result = $repository->findByuserid($id);
+
+
+        return $this->render('arts/profile.html.twig', array('results' => $result));
+    }
 
 }
--- WebProjectSym2.8/src/AppBundle/Entity/Arts.php
+++ WebProjectSym2.8/src/AppBundle/Entity/Arts.php
@@ -66,6 +66,120 @@ class Arts
      */
     private $userid;
 
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNaam()
+    {
+        return $this->naam;
+    }
+
+    /**
+     * @param string $naam
+     */
+    public function setNaam($naam)
+    {
+        $this->naam = $naam;
+    }
+
+    /**
+     * @return string
+     */
+    public function getActhernaam()
+    {
+        return $this->acthernaam;
+    }
+
+    /**
+     * @param string $acthernaam
+     */
+    public function setActhernaam($acthernaam)
+    {
+        $this->acthernaam = $acthernaam;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAdress()
+    {
+        return $this->adress;
+    }
+
+    /**
+     * @param string $adress
+     */
+    public function setAdress($adress)
+    {
+        $this->adress = $adress;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProfielfoto()
+    {
+        return $this->profielfoto;
+    }
+
+    /**
+     * @param string $profielfoto
+     */
+    public function setProfielfoto($profielfoto)
+    {
+        $this->profielfoto = $profielfoto;
+    }
+
+    /**
+     * @return \User
+     */
+    public function getUserid()
+    {
+        return $this->userid;
+    }
+
+    /**
+     * @param \User $userid
+     */
+    public function setUserid($userid)
+    {
+        $this->userid = $userid;
+    }
+
+
+
 
 }
 
